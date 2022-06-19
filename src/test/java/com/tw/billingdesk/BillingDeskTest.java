eb6ed240dae8ffbf456f0412412b636d5ea2fb83
Shelly Suri <shelly.suri99@gmail.com>
2022-06-19 10:46:54 +0530

    Add. Test case for input 2

--- src/test/java/com/tw/billingdesk/BillingDeskTest.java
+++ src/test/java/com/tw/billingdesk/BillingDeskTest.java
@@ -6,11 +6,19 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class BillingDeskTest {
     @Test
-    public void ableToAddItemsToCart(){
+    public void generateReceiptForInput1(){
         BillingDesk billingDesk = new BillingDesk();
         billingDesk.addToCart(ItemType.Book, "Book", 12.49, 1, false);
         billingDesk.addToCart(ItemType.Miscellaneous, "music CD", 14.99, 1, false);
         billingDesk.addToCart(ItemType.Food, "chocolate bar", 0.85, 1, false);
+        billingDesk.generateReceipt();
+    }
+
+    @Test
+    public void generateReceiptForInput2(){
+        BillingDesk billingDesk = new BillingDesk();
+        billingDesk.addToCart(ItemType.Food, "imported box of chocolates", 10.00, 1, true);
+        billingDesk.addToCart(ItemType.Miscellaneous, "imported box of perfume", 47.50, 1, true);
         billingDesk.generateReceipt();
     }
 }
