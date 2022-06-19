01848f5efa716f22dbcb7359febbce06adc05786
Shelly Suri <shelly.suri99@gmail.com>
2022-06-19 10:52:41 +0530

    Add. Test case for input 3

--- src/test/java/com/tw/billingdesk/BillingDeskTest.java
+++ src/test/java/com/tw/billingdesk/BillingDeskTest.java
@@ -18,7 +18,17 @@ class BillingDeskTest {
     public void generateReceiptForInput2(){
         BillingDesk billingDesk = new BillingDesk();
         billingDesk.addToCart(ItemType.Food, "imported box of chocolates", 10.00, 1, true);
-        billingDesk.addToCart(ItemType.Miscellaneous, "imported box of perfume", 47.50, 1, true);
+        billingDesk.addToCart(ItemType.Miscellaneous, "imported bottle of perfume", 47.50, 1, true);
+        billingDesk.generateReceipt();
+    }
+
+    @Test
+    public void generateReceiptForInput3(){
+        BillingDesk billingDesk = new BillingDesk();
+        billingDesk.addToCart(ItemType.Miscellaneous, "imported bottle of perfume", 27.99, 1, true);
+        billingDesk.addToCart(ItemType.Miscellaneous, "bottle of perfume", 18.99, 1, true);
+        billingDesk.addToCart(ItemType.Medical, "packet of headache pills", 9.75, 1, false);
+        billingDesk.addToCart(ItemType.Food, "imported box of chocolates", 11.25, 1, true);
         billingDesk.generateReceipt();
     }
 }
